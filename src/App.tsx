9154c02cfa8c090c6a6b3ab314dface18244583f
rkrb3112 <rkrb3112@gmail.com>
2024-03-17 13:39:08 +0530

    file changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Person, Country} from "./components/Person"
 
 function App() {
+  // const getAge = (name: string): number =>{
+  //   return 99
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name="raj" email="raj@gmail.com" age={28} isMarried={false} friends={["assad", "dsvds", "aaDDAS"]} country={Country.Europe} />
     </div>
   );
 }
